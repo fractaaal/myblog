3199110df7fbbf521a91c585c61b9fba228dc80e
fractaaal <b1329180@gmail.com>
2021-05-18 21:10:12 +0900

    20210518 add all-posts

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import AllPost from '/posts/all-posts'
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
           <a>First Post</a>
         </Link>
       </section>
+      <AllPost/>
     </Layout>
   )
 }
---
+++ pages/posts/all-posts.js
@@ -0,0 +1,30 @@
+// posts はビルド時に getStaticProps() によって生成されます。
+function Blog({ posts }) {
+    return (
+      <ul>
+        {posts.map(post => (
+          <li>{post.title}</li>
+        ))}
+      </ul>
+    );
+}
+  
+  // この関数はサーバー側でのビルド時に呼び出されます。
+  // クライアント側では呼び出されないため、データベースクエリを直接実行することも可能です。
+  // 「技術詳細」のセクションをご覧ください。
+export async function getStaticProps() {
+    // 外部のAPIエンドポイントを呼び出してpostsを取得します。
+    // 任意のデータ取得ライブラリを使用できます。
+    const res = await fetch('https://qiita.com//api/v2/users/Sota_Matsui/items?page=1&per_page=100');
+    const posts = await res.json();
+  
+    // { props: posts } を返すことで、Blog コンポーネントは
+    // ビルド時に`posts`を prop として受け取ります。
+    return {
+      props: {
+        posts
+      }
+    };
+}
+
+export default Blog;

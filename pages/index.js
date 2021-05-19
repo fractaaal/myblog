51d5a1f05ea842b86ee99301eb3d6150cebbbc53
fractaaal <b1329180@gmail.com>
2021-05-19 21:28:21 +0900

    20210519 use getServerSideProps()

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
-import AllPost from '/posts/all-posts'
+import AllPosts from './posts/all-posts'
+
 
 export default function Home() {
   return (
@@ -17,7 +18,7 @@ export default function Home() {
           <a>First Post</a>
         </Link>
       </section>
-      <AllPost/>
+      <AllPosts/>
     </Layout>
   )
 }
--- pages/posts/all-posts.js
+++ pages/posts/all-posts.js
@@ -1,10 +1,11 @@
+import { getStaticProps } from 'next'
+
 // posts はビルド時に getStaticProps() によって生成されます。
-function Blog({ posts }) {
+function AllPosts({ posts }) {
     return (
       <ul>
-        {posts.map(post => (
-          <li>{post.title}</li>
-        ))}
+          <li>{posts}</li>
+
       </ul>
     );
 }
@@ -12,7 +13,7 @@ function Blog({ posts }) {
   // この関数はサーバー側でのビルド時に呼び出されます。
   // クライアント側では呼び出されないため、データベースクエリを直接実行することも可能です。
   // 「技術詳細」のセクションをご覧ください。
-export async function getStaticProps() {
+export async function getServerSideProps() {
     // 外部のAPIエンドポイントを呼び出してpostsを取得します。
     // 任意のデータ取得ライブラリを使用できます。
     const res = await fetch('https://qiita.com//api/v2/users/Sota_Matsui/items?page=1&per_page=100');
@@ -27,4 +28,4 @@ export async function getStaticProps() {
     };
 }
 
-export default Blog;
+export default AllPosts;

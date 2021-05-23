f866b89a256d06151c079e44909eeec98d60caaa
fractaaal <b1329180@gmail.com>
2021-05-23 18:24:42 +0900

    20210532 use utf-8 for post contents

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -15,7 +15,7 @@ export default function Post({ postData }) {
                 <div className={utilStyles.lightText}>
                   <Date dateString={postData.date} />
                 </div>
-                <p>{postData.fileContents}</p>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
             </article>
         </Layout>
     )

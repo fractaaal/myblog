5d16d891b6408d5288a65b7202bc0ed5e871282f
fractaaal <b1329180@gmail.com>
2021-05-26 18:00:21 +0900

    20210526 use kentax

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import 'highlight.js/styles/default.css';
+import "katex/dist/katex.min.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

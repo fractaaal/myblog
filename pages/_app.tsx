49f3880376f0cf9e57398f38000b10af2b963ba9
fractaaal <b1329180@gmail.com>
2021-05-31 19:54:59 +0900

    20210531 edit prism

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import "katex/dist/katex.min.css"
+import 'highlight.js/styles/tomorrow-night.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
